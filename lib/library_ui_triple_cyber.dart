7ac5f10d272b3a7bd6cf6724d9d5fd43be6191d5
sergiopacheco <sergio.pacheco@triplecyber.com.do>
2025-07-17 14:59:39 -0400

    kjnkj

--- lib/library_ui_triple_cyber.dart
+++ lib/library_ui_triple_cyber.dart
@@ -2,3 +2,9 @@ library library_ui_triple_cyber;
 
 // Export
 export 'identity_score/identity_score_page.dart';
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
